f7ee44ce602c211018ce6fee4088b0cf70e10bbc
fonzerelly <ChristianHoerauf@web.de>
2015-06-21 22:09:26 +0200

    create index.html

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,24 @@ gulp = require('gulp'),
 concat = require('gulp-concat');
 
 var app = [
+    './bower_components/angular/angular.js',
     './app/**/*.js'
 ];
 
-gulp.task('deploy', function() {
+var index = [
+    './app/index.html'
+];
+
+gulp.task('deployJS', function() {
     return gulp.src(app)
         .pipe(concat('myApp.js'))
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('default', ['deploy']);
+gulp.task('deployHTML', function () {
+    return gulp.src(index)
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', ['deployJS', 'deployHTML']);
 
